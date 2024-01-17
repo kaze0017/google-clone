95b3ed38fc20ac01c6d246d62e7e8c0a539c2fff
Keivan Kazemi <104168492+kaze0017@users.noreply.github.com>
2024-01-17 18:21:22 -0500

    adds header

---
+++ src/app/head.tsx
@@ -0,0 +1,6 @@
+<link
+  rel="icon"
+  href="/favicon.ico"
+  type="image/<generated>"
+  sizes="<generated>"
+/>;
---
+++ src/app/shared-metadata.js
@@ -0,0 +1,1 @@
+export const openGraphImage = { images: ["/images/open-graph.png"] };
