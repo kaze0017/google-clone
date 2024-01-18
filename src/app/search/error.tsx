fdcfe513edfb73294da172347c0b77bf339ea2fe
Keivan Kazemi <104168492+kaze0017@users.noreply.github.com>
2024-01-18 16:09:22 -0500

    removes useeffect from error

--- src/app/search/error.tsx
+++ src/app/search/error.tsx
@@ -1,10 +1,7 @@
 "use client";
-import React, { useEffect } from "react";
 
 export default function error({ error, reset }: { error: any; reset: any }) {
-  useEffect(() => {
-    console.log("error: ", error);
-  }, [error]);
+
   return (
     <div className="flex flex-col items-center justify-center   pt-10">
       <h1 className="text-3xl mb-4">Something went wrong.</h1>
