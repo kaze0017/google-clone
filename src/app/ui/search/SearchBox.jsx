9d8545fbd12ded8d6d527bd77aeeaec16f4b95e9
Keivan Kazemi <104168492+kaze0017@users.noreply.github.com>
2024-01-17 22:56:28 -0500

    adds searchparams

--- src/app/ui/search/SearchBox.jsx
+++ src/app/ui/search/SearchBox.jsx
@@ -1,5 +1,59 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
+import { RxCross2 } from "react-icons/rx";
+import { FaMicrophone } from "react-icons/fa";
+import { AiOutlineSearch } from "react-icons/ai";
+import {
+  useSearchParams,
+  usePathname,
+  useRouter,
+  useParams,
+} from "next/navigation";
 
 export default function SearchBox() {
-  return <div>SearchBox</div>;
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+  const params = new URLSearchParams(searchParams);
+  const [query, setQuery] = React.useState(params.get("search"));
+
+  function handleQuery(query) {
+    setQuery(query);
+  }
+  function deleteQuery() {
+    setQuery("");
+  }
+
+  function handleSubmit() {
+    console.log(query);
+    const params = new URLSearchParams(searchParams);
+    params.set("search", query);
+    replace(`${pathname}?${params.toString()}`);
+  }
+
+  return (
+    <form
+      className="flex items-center border border-gray-200 rounded-full shadow-lg px-6 py-1 ml-10 mr-5 flex-grow max-w-3xl"
+      onSubmit={(e) => {
+        e.preventDefault();
+        handleSubmit();
+      }}
+    >
+      <input
+        type="text"
+        className="w-full focus:outline-none "
+        value={query}
+        onChange={(e) => handleQuery(e.target.value)}
+      />
+      <RxCross2
+        className="text-2xl text-gray-500 cursor-pointer sm:mr-2"
+        onClick={deleteQuery}
+      />
+      <FaMicrophone className="hidden sm:inline-flex text-4xl text-blue-500 pl-4 border-l-2 border-gray-300 mr-3" />
+      <AiOutlineSearch
+        className="text-2xl hidden sm:inline-flex text-blue-500 cursor-pointer"
+        onClick={() => handleSubmit()}
+      />
+    </form>
+  );
 }
